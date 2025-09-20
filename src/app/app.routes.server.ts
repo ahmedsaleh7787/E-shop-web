44e972f0dc7d6829f28cd33d1c9c9529f46f0d32
ahmedsaleh7787 <ahmed.saleh7787@gmail.com>
2025-09-20 18:36:08 +0300

    Assignment submission v1 + app.routes.server.ts change

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -1,8 +1,23 @@
 import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
+
+    {
+    path: 'details/:slug/:id',
+    renderMode: RenderMode.Server
+  },
+  {
+    path: 'details/:id',
+    renderMode: RenderMode.Server
+  },
+  {
+    path: 'checkout/:cartId',
+    renderMode: RenderMode.Server
+  },
+
   {
     path: '**',
     renderMode: RenderMode.Prerender
   }
+
 ];
