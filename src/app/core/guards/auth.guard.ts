3145dcca748afb5616dcb983fd4f8c09b18a21e4
ahmedsaleh7787 <ahmed.saleh7787@gmail.com>
2025-09-20 18:58:21 +0300

    Assignment submission v2 build

--- src/app/core/guards/auth.guard.ts
+++ src/app/core/guards/auth.guard.ts
@@ -2,12 +2,19 @@
 import { inject } from '@angular/core';
 import { CanActivateFn, Router } from '@angular/router';
 import {CookieService} from 'ngx-cookie-service'; 
+import { PLATFORM_ID } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
+
 
 export const authGuard: CanActivateFn = (route, state) => {
 
 const router:Router=inject(Router)
 const cookieService=inject(CookieService) 
+const platformId = inject(PLATFORM_ID);
 
+  if (!isPlatformBrowser(platformId)) {
+    return true
+  }
 
 
   if(cookieService.get("token")){
